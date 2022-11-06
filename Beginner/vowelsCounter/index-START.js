3ef7c7543b4d09339d152cf47db4dceebc19ea88
Kevin <klzheng@buffalo.edu>
2022-11-06 12:14:58 -0500

    vowels counter - completed

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -4,8 +4,30 @@ e.g vowelsCounter('anehizxcv') // will return 3
 */
 
 
+
+
+
+// fn that takes in string and returns # of vowels
+
+// string -> length > 0, capital letters, 5 vowels to consider ("aeiou")
+// return # of vowels -> integer, length >= 0, 
+
+// "aeiouy" -> 6
+// "vowels" -> 2
+// "hmmm" -> 0
+
+
 function vowelsCounter(text) {
-    // Code goes here
+    // store the vowels in an arr
+    const vowels = ["a","e",'i','o','u']
+    // initialize counter
+    let counter = 0
+    // loop through string and check if char is in vowels arr
+    for (let char of text.toLowerCase()) {
+        // if true increase counter
+        if (vowels.includes(char)) counter++
+    }
+    return counter
 }
 
 
