07988cde1a72cbc5a0ad4c471d498b3909b91d35
Kevin <klzheng@buffalo.edu>
2022-11-06 22:12:18 -0500

    palindromeChecker - completed

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -6,9 +6,38 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
+// fn that takes in string and returns boolean depending on if string is palindrome
+
+// string -> no empty string, at least 1 character, no special chars, numbers included
+
+// return boolean -> true, false
+
+
+
+// "racecar" -> true
+// "leon noel" -> true
+// "asd" -> false
+
+
+
 function palindromeChecker(text) {
-    v// Code goes here
+    const reversedText = text.split("").reverse().join("")
+    return reversedText === text
 }
+
+
+// function palindromeChecker(text) {
+//     // initialize empty string
+//     let reversedString = ""
+//     // loop through original string in reverse order
+//     for (let i = text.length-1; i >= 0; i--) {
+//         // add each letter to empty string
+//         reversedString += text[i]
+//     }
+//     // check if the reversed string === original string
+//     // return result
+//     return reversedString === text
+// }
 
 
 
