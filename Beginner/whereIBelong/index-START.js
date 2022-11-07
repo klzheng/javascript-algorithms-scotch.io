dff5d11546d670073ccb0062947b37f4ed27c7a6
Kevin <klzheng@buffalo.edu>
2022-11-07 14:06:01 -0500

    whereIBelong - completed

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -8,11 +8,25 @@ than 1(index 0), but less than 2(index 1).
 */
 
 
+// fn takes in a sorted arr of nums, and a value - returns the largest index where the value would be inserted while maintaining sorted arr
+
+// array of nums, val -> sorted
+
+// return index -> number
+
 
 function whereIBelong(arr, num) {
-   // Code goes here
-
+   // initialize return index
+   let index = 0
+   // loop through given sorted arr
+   if (arr.length === 0) return index
+   for (let i = 0; i < arr.length; i++){
+      // check if num < item in arr
+      // if true, return index
+      if (num <= arr[i]) return i
+   }
 }
+
 
 
 
