b7f525086bb9debbb526d5f106e8f88b2ca1cf90
Kevin <klzheng@buffalo.edu>
2022-11-06 21:47:46 -0500

    capSentence - completed

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -7,9 +7,32 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 
+
+// fn that takes in a string of >= 2 words, and returns the same string but each word is capitalized (first letter uppercase)
+
+// sentence -> string, length >= 2, separated by spaces
+
+// return sentence -> string, first letter of every word capital
+
+
+// "hi my name is kevin" -> "Hi My Name Is Kevin"
+// "the browser" -> "The Browser"
+// "12 !#@" -> "12 !#@"
+
+
 function capSentence(text) {
-   // Code goes here
+  // split text by spaces
+  let textArray = text.toLowerCase().split(" ")
+  // loop through arr of words and capitalize the first letter of each word
+  let capitalized = textArray.map(word => word[0].toUpperCase() + word.slice(1))
+  // join words in arr to form string and return string
+  return capitalized.join(" ")
 }
+
+
+// console.log(capSentence("hi my name is kevin"), "Hi My Name Is Kevin")
+// console.log(capSentence("the browser"), "The Browser")
+// console.log(capSentence("12 !#@"), "12 !#@")
 
 
 
